65a82756181cfbedad59720812b531d3a77f13c3
Harshit Raj Sinha <74960032+harshitrajsinha@users.noreply.github.com>
2024-08-09 15:35:13 +0530

    Update miscellaneous.js

--- miscellaneous.js
+++ miscellaneous.js
@@ -1,2 +1,14 @@
 // Convert a number to binary
 const to_binary = num.toString(2);
+
+// --------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// setTimeout - used to execute a process after certain time interval
+const print_my_name = function(){   // this type of syntax for writing function is used when we want to give reference of the function to elsewhere
+  console.log("harshit");
+}
+setTimeout(print_my_name, 2000);
+// Note: Above we are giving reference of the function
+// setTimeout(print_my_name(), 2000);  :- this will execute the function immediately
+
+// --------------------------------------------------------------------------------------------------------------------------------------------------------------
