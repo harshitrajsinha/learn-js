ab6117a6cc61c132814db607a163f9b79a46c56d
Harshit Raj Sinha <74960032+harshitrajsinha@users.noreply.github.com>
2024-08-26 19:50:56 +0530

    Create functions.js

---
+++ Functions/functions.js
@@ -0,0 +1,19 @@
+// Example: 1
+
+function updateArg(param){
+  if(typeof param === 'object'){
+    mycar[Object.keys(param)[0]] = 'change';
+  }else{
+    param = 23; 
+  }
+}
+let argNum = 4;
+updateArg(argNum);
+console.log(argNum); // 4
+let mycar = {
+  make: "Honda",
+  model: "Accord",
+  year: 1998,
+};
+updateArg(mycar);
+console.log(mycar.make); // "change"
