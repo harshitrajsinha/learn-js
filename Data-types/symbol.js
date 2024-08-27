f9b1aad6f9e72298799232ea19fce48dc6ec6261
Harshit Raj Sinha <74960032+harshitrajsinha@users.noreply.github.com>
2024-08-27 19:03:39 +0530

    Update symbol.js

--- Data-types/symbol.js
+++ Data-types/symbol.js
@@ -8,3 +8,16 @@ console.log(symType === symType2); // false
 const sym1 = Symbol.for('desc');
 const sym2 = Symbol.for('desc');
 console.log(sym1 === sym2); // true
+
+/* Example - 2 */
+let symbolType = Symbol('fn');
+function changeValue(param){
+    console.log(param.description); // 'fn'
+    if(typeof param === 'symbol'){
+        param = Symbol('fnn');
+    }
+}
+console.log(symbolType, typeof symbolType); // Symbol(fn) symbol
+changeValue(symbolType);
+console.log(symbolType, typeof symbolType); // Symbol(fn) symbol
+// Explaination: Copy of reference will be passed => symbolType, param will poiint to same symbol in heap memory but due to uniqueness and immutability, when param will be assigned to a new symbol then instead of modifying the previous symbol, a new symbol will be created and param will now point to the new symbol and the previous symbol will remain as it is.
