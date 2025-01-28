af7a5b75d497af24489d3a7df2876811814f6f37
Harshit Raj Sinha <raj.harshitsinha08@gmail.com>
2025-01-28 15:13:37 +0530

    Update functions.js

--- Functions/functions.js
+++ Functions/functions.js
@@ -17,3 +17,48 @@ let mycar = {
 updateArg(mycar);
 console.log(mycar.make); // "change"
 // Above example shows non-primitve data types are passed by reference, but primitves are passed by value.
+
+
+
+
+
+
+function mul(x) {
+  // there can be a heavy CPU-intensive job here
+  console.log(`Called mul with ${x}`);
+  return x * 5;
+}
+
+function cachingDecorator(func) {
+  let cache = new Map();
+
+  return function (x) {
+    if (cache.has(x)) {
+      // if there's such key in cache
+      console.log(cache);
+      return cache.get(x); // read the result from it
+    }
+
+    let result = func(x); // otherwise call func
+
+    cache.set(x, result); // and cache (remember) the result
+    console.log(cache);
+    return result;
+  };
+}
+
+addNum = cachingDecorator(add);
+mulNum = cachingDecorator(mul);
+
+console.log(addNum(1));
+console.log(addNum(2));
+console.log(addNum(1));
+console.log(mulNum(1));
+console.log(mulNum(2));
+console.log(mulNum(1));
+console.log(addNum(1));
+
+
+
+
+
